444d1837728564f9e8b0081864bae5abb9a2b44a
mtbthebest <mtbthebest11@gmail.com>
2021-07-27 05:39:20 +0900

    c17

---
+++ partIV/src/p4.cpp
@@ -0,0 +1,16 @@
+//
+// Created by mtb on 7/14/21.
+//
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<list>
+#include <complex>
+using namespace std;
+
+int main(int argc, char **argv){
+
+    return 0;
+}
